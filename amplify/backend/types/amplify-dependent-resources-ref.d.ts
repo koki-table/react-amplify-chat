d6d5df8701b1c1deaf4be7f16fef1d3916325e32
koki-shino <shino.k@x-point-1.net>
2023-01-08 16:49:39 +0900

    add: api

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,10 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "reactamplifychat": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string"
+    }
+  },
   "auth": {
     "reactamplifychatdcc94fc5": {
       "AppClientID": "string",
